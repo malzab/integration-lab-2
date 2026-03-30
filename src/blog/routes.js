167d658efb8a1988710a9f63ab0f363950992765
Malwina Zabielska <malwinazabielska1@gmail.com>
2026-03-30 23:24:42 +0200

    Initialize database and basic administrative access

--- src/blog/routes.js
+++ src/blog/routes.js
@@ -2,5 +2,15 @@ const express = require('express');
 const router = express.Router();
 const Post = require('./post')
 
+router.post('/nowy', async (req, res) => {
+  try {
+    const { title, content, author } = req.body;
+    await Post.create({ title, content, author});
+    res.send('<p>Post dodany!</p>');
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Błąd');
+  }
+});
 
 module.exports = router;
--- src/index.js
+++ src/index.js
@@ -4,5 +4,12 @@ const app = express();
 const sequelize = require('./config/baza');
 require('./blog/post');
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+sequelize.sync();
+
 const blogSciezki = require('./blog/routes');
 app.use('/blog', blogSciezki);
+
+app.listen(3000, () => console.log('Serwer działa na http://localhost:3000'));
